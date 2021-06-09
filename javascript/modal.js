bef73d32906f6e98540bfdc7a63fdb758eea7bd8
xzzhan4 <xzzhan4@student.unimelb.edu.au>
2021-06-09 22:28:36 +1000

    modal and cart button

---
+++ javascript/modal.js
@@ -0,0 +1,11 @@
+function openModal(){
+    document.getElementById("modal--itemadded").style.display="block";
+}
+
+function closeModal(){
+    document.getElementById("modal--itemadded").style.display="none";
+}
+
+function showdropdown(){
+    document.getElementById("normalcart").style.zIndex="2";
+}
